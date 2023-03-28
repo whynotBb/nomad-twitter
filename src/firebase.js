33ebc45eb4215d5da3c3655fdfa5ece9cb8ec316
bb <whynot.byoul@gmail.com>
2023-03-29 08:31:04 +0900

    firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDrgZ6AzzgvHb61GOf5T-YT1-HMocxxNk8",
+  authDomain: "nomad-twitter-4827d.firebaseapp.com",
+  projectId: "nomad-twitter-4827d",
+  storageBucket: "nomad-twitter-4827d.appspot.com",
+  messagingSenderId: "442732989307",
+  appId: "1:442732989307:web:ed46fd1143058ac4078bde",
+};
+
+export default firebase.initializeApp(firebaseConfig);
