0de9ff8072f273b8eda8de5d289bef8209ac1eb9
bb <whynot.byoul@gmail.com>
2023-03-30 07:49:49 +0900

    router

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,13 @@
 import * as firebase from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDrgZ6AzzgvHb61GOf5T-YT1-HMocxxNk8",
-  authDomain: "nomad-twitter-4827d.firebaseapp.com",
-  projectId: "nomad-twitter-4827d",
-  storageBucket: "nomad-twitter-4827d.appspot.com",
-  messagingSenderId: "442732989307",
-  appId: "1:442732989307:web:ed46fd1143058ac4078bde",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 export default firebase.initializeApp(firebaseConfig);
